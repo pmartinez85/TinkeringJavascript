6f5b88761d56b90851142bb2b80b100c2570c8b7
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-05 19:44:13 +0200

    objecte Persona v1

--- objectes.js
+++ objectes.js
@@ -7,3 +7,21 @@
 // First clas citizens
 // var x = function (){};
 // javascript: function programming
+
+
+//Funcions constructores
+
+function Persona(){
+
+        console.log('crear una nova persona');
+        this.name='Pedro';
+        this.sn1='Martinez';
+}
+
+var Persona1 = new Persona();
+var Persona2 = new Persona();
+console.log(Persona1);
+
+
+// Mirar què fa el new exactament...no es un objecte nou
+
