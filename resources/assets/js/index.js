21b7b0dff5641dd3036d78301bfc53b951f14418
Pedro Martínez <pmartinez1085@gmail.com>
2016-11-26 18:09:47 +0100

    refactor

---
+++ resources/assets/js/index.js
@@ -0,0 +1,15 @@
+//javascript tradicional
+//DOM manipulation
+//DOM: API : Aplicattion Programming Interface
+//XML i HTML_ API (SAX)
+//
+//
+// var hello = document.getElementById("hello").innerHTML = "hola soc Pedro";
+//
+// hello = document.getElementById("hello");
+//
+// console.log(hello);
+
+//Node: Global node iasdad.js
+
+document.getElementById("prova").innerHTML = 'Hola!';
